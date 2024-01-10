d71b8099c7ca6d2e7c3254ab1d9b21b10ba507ed
Jason Copp <jason.copp@bloomerang.com>
2024-01-09 19:49:06 -0500

    om4 wip

--- ble-dl200.js
+++ ble-dl200.js
@@ -355,7 +355,7 @@ function disconnect() {
 	if (bleDolly) {
 		// disconnect:
 		bleDolly.gatt.disconnect();
-		uidisablement(disabled=true);
+		uidisablement(true);
 	}
 }
 
@@ -490,14 +490,190 @@ window.onunload = async function () {
 }
 
 setTimeout(function() {
-	uidisablement(disabled=true); // my distain for double negatives is stronger than my distain for the fact that this works. actually, i rather like this <--
-},1)
+	crcsetup();
+	uidisablement(true); // my distain for double negatives is stronger than my distain for the fact that this works. actually, i rather like this <--
+	let s = performance.now();
+	log(`time: ${s}`)
+	checkDJICommand('0x551504a90204e18c00040c0700000000008100')
+	let e = performance.now();
+	log(`time: ${e-s}`)
+	checkDJICommand('0x551504a90204e18c00040c0733000000008100')
+	let a = performance.now();
+	log(`time: ${a-e}`)
+},200)
+
+let crc;
+function crcsetup() {
+	var width = 16;
+	var polynomial = 0x1021;
+	var initial = 0x496c;
+	var finalXor = 0x0000;
+	var inputReflected = true;
+	var resultReflected = true;
+
+	//Execution
+	crc = new Crc({width: width, polynomial: polynomial, initial: initial, finalXor : finalXor, inputReflected: inputReflected, resultReflected : resultReflected });
+}
+
+function checkDJICommand(commandAsString){
+	// bytearray = hexStringToByteArray('0x551504a90204e18c00040ce0ff000000007f00')
+	let bytearray = hexStringToByteArray(commandAsString)
+	let checksum = crc.compute(bytearray);
+	let hexresult = decimalToHex(checksum)
+	hexresult = hexresult.slice(2,4) + hexresult.slice(0,2);
+	log(`Checked Result: ${commandAsString}${hexresult}`)
+	return `${commandAsString}${hexresult}`
+
+}
 
 async function uidisablement(disabled) {
 	var buttons = [...document.querySelectorAll("button"), ...document.querySelectorAll("input")]; 
 	for(var i = 0, len = buttons.length; i < len; i++) {  
-		button = buttons[i]
+		let button = buttons[i]
 		button.disabled = disabled;
 	}
 	document.getElementById("connect").disabled = !disabled;
 }
+
+function calculateCRC16(data,poly,init,finalXOR) {
+    const polynomial = poly || 0x1021;
+    let crc = init || 0x496c;
+	let final = finalXOR || 0x0000;
+
+    for (let i = 0; i < data.length; i++) {
+        crc ^= (data[i] << 8);
+
+        for (let j = 0; j < 8; j++) {
+            crc = (crc & 0x8000) ? ((crc << 1) ^ polynomial) : (crc << 1);
+        }
+    }
+
+    return crc & final;
+}
+
+function hexStringToByteArray(hexString) {
+	hexString = hexString.replace("0x", "");
+    if (hexString.length % 2 !== 0) {
+        throw "Must have an even number of hex digits to convert to bytes";
+    }
+    var numBytes = hexString.length / 2;
+	var byteArray = new Uint8Array(numBytes);
+    for (var i=0; i<numBytes; i++) {
+	    byteArray[i] = parseInt(hexString.substr(i*2, 2), 16);
+    }
+    return byteArray;
+}
+
+const decimalToHex = dec => dec.toString(16);
+
+function go() {
+	bytearray = hexStringToByteArray('0x551504a90204e18c00040ce0ff000000007f00')
+	crc = calculateCRC16(bytearray)
+	xcrc = decimalToHex(crc)
+	console.log(xcrc)
+}
+
+
+/**
+ * Based On:
+ * crc-js
+ * Created by Johannes Rudolph on 09.12.2016.
+ *
+ * Based On:
+ * Implementation to calculate the CRC value for a given string / string of bytes.
+ * Sunshine, May 2k15
+ * www.sunshine2k.de || www.bastian-molkenthin.de
+ */
+
+
+var Crc = function (options) {
+	this.initialize (options);
+  };
+  
+  Crc.prototype = {
+  
+	version: "2.0.0",
+  
+	initialize: function (options) {
+			this._width = options.width;
+			this._polynomial = options.polynomial;
+			this._initialVal = options.initial;
+			this._finalXorVal = options.finalXor;
+			this._inputReflected = options.inputReflected;
+			this._resultReflected = options.resultReflected;
+  
+  
+		switch (this._width)
+		{
+			case 8: this._castMask = 0xFF; break;
+			case 16: this._castMask = 0xFFFF; break;
+			case 32: this._castMask = 0xFFFFFFFF; break;
+			default: throw "Invalid CRC width";
+		}
+  
+		this._msbMask = 0x01 << (this._width - 1);
+  
+		this.calcCrcTable();
+	},
+  
+	calcCrcTable: function(){
+		this._crcTable = new Array(256);
+  
+		for (var divident = 0; divident < 256; divident++)
+		{
+			var currByte = (divident << (this._width - 8)) & this._castMask;
+			for (var bit = 0; bit < 8; bit++)
+			{
+				if ((currByte & this._msbMask) !== 0)
+				{
+					currByte <<= 1;
+					currByte ^= this._polynomial;
+				}
+				else
+				{
+					currByte <<= 1;
+				}
+			}
+			this._crcTable[divident] = (currByte & this._castMask);
+  
+		}
+	},
+  
+	compute : function (bytes){
+		var crc = this._initialVal;
+		for (var i = 0; i < bytes.length; i++){
+  
+			var curByte = bytes[i] & 0xFF;
+  
+			if (this._inputReflected){
+				curByte = this.reflect(curByte, 8);
+			}
+  
+			/* update the MSB of crc value with next input byte */
+			crc = (crc ^ (curByte << (this._width - 8))) & this._castMask;
+			/* this MSB byte value is the index into the lookup table */
+			var pos = (crc >> (this._width - 8)) & 0xFF;
+			/* shift out this index */
+			crc = (crc << 8) & this._castMask;
+			/* XOR-in remainder from lookup table using the calculated index */
+			crc = (crc ^ this._crcTable[pos]) & this._castMask;
+		}
+  
+		if (this._resultReflected) {
+			crc = this.reflect(crc, this._width);
+		}
+		return ((crc ^ this._finalXorVal) & this._castMask);
+	},
+  
+	reflect: function (val, width) {
+		var resByte = 0;
+  
+		for (var i = 0; i < width; i++) {
+			if ((val & (1 << i)) !== 0) {
+				resByte |= (1 << ((width-1) - i));
+			}
+		}
+  
+		return resByte;
+	}
+  };
